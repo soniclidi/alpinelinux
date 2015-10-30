b567389cac28ff38b67d8e43caf2ed2aae50268e
soniclidi <soniclidi@gmail.com>
2015-10-30 14:58:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-rc-update add sshd
-rc-status
-touch /run/openrc/softlevel
-
 if [ ! -f /.root_pw_set ]; then
 	sh /set_root_pw.sh
 fi
 
-/etc/init.d/sshd start
-while true;do sleep 1;done
+rc-status
+touch /run/openrc/softlevel
+ssh-keygen -A
+
+exec /usr/sbin/sshd -D
